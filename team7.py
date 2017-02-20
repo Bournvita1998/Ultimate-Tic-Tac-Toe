aaef72973f2c10bfb9ee63d49194288448357cc8
patniharshit <patniharshit@gmail.com>
2017-02-20 23:23:37 +0530

    define move and check_utility functions

--- team7.py
+++ team7.py
@@ -10,3 +10,9 @@ class Player7:
 
 	def minimax(self,old_move, depth, max_depth, alpha, beta, isMax, p_board, p_block, flag1, flag2, best_node):
 		pass
+	
+	def check_utility(self,block,board) :
+		pass
+
+	def move(self,board,old_move,flag1) :
+		pass
