11eb29cdd4e995960079b3d766eaa517543f327d
patniharshit <patniharshit@gmail.com>
2017-02-20 23:48:45 +0530

    write random heuristic and move on board

--- team7.py
+++ team7.py
@@ -12,7 +12,17 @@ class Player7:
 		pass
 	
 	def check_utility(self,block,board) :
-		pass
+		a = random.randint(-50,50)
+		#print a
+		return a
 
 	def move(self,board,old_move,flag1) :
-		pass
+		if flag1 == 'x' :
+			flag2 = 'o'
+		else :
+			flag2 = 'x'
+		(utility_value, best_node) = self.minimax(old_move,0,5,self.ALPHA,self.BETA,True,board, (1,1), flag1, flag2, (7,7))
+		#return (int(best_node[0]), int(best_node[1]))
+		# print utility_value
+		# print best_node
+		return best_node
