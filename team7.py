84daa727a3b99e60099db1d24bcc9f5be9edb56c
patniharshit <patniharshit@gmail.com>
2017-02-20 23:17:05 +0530

    initialize player class

---
+++ team7.py
@@ -0,0 +1,12 @@
+from simulator import *
+import random
+
+class Player7:
+
+	def __init__(self):
+		#print "entry 1"
+		self.ALPHA = -100000000
+		self.BETA = 100000000
+
+	def minimax(self,old_move, depth, max_depth, alpha, beta, isMax, p_board, p_block, flag1, flag2, best_node):
+		pass
